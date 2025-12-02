2d1e1c96138b4e0c606235cc620289855c290207
jeonghuny <wnele@naver.com>
2025-12-02 16:15:00 +0900

    fix

--- git_syntax/1.env_settings.sh
+++ git_syntax/1.env_settings.sh
@@ -118,4 +118,7 @@ git remote add origin https://github.com/jeonghuny/test2.git
 git checkout -b main # main 브랜치 생성 및 변경
 # git add, git commit, git push 작업 진행
 
-
+# .gitignore 파일은 git추적목록에서 제외 대상 나열
+# 주로 빌드된 파일 또는 중요암호 등이 담긴 파일을 제외한다.
+# 만약 이미 git에서 추적되고 있는 파일을 제외하려면 캐시 삭제 필요
+git rm -r --cached . 
